79b246b71464daf6a8bdc3a9591787c869873891
aarontang <aarontang@tencent.com>
2017-03-31 15:01:58 +0800

    [run]milestone: 目前LSD算法运行正常，前5帧得出的pose数据与PC上的基本一致

--- app/src/main/java/com/tc/tar/MainActivity.java
+++ app/src/main/java/com/tc/tar/MainActivity.java
@@ -1,11 +1,18 @@
 package com.tc.tar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.opengl.GLES10;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainActivity extends AppCompatActivity implements GLSurfaceView.Renderer {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    private GLSurfaceView mGLSurfaceView;
 
     static {
         System.loadLibrary("g2o_core");
@@ -31,5 +38,34 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         TARNativeInterface.nativeInit();
+        mGLSurfaceView = new GLSurfaceView(this);
+        mGLSurfaceView.setEGLContextClientVersion(2);
+        mGLSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        mGLSurfaceView.setPreserveEGLContextOnPause(true);
+        mGLSurfaceView.setRenderer(this);
+//        mGLSurfaceView.setOnTouchListener(this);
+        mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        //mGLView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+        setContentView(mGLSurfaceView);
+
+        registerForContextMenu(mGLSurfaceView);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        TARNativeInterface.nativeInitGL();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        TARNativeInterface.nativeResize(width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glClear(GLES10.GL_COLOR_BUFFER_BIT | GLES10.GL_DEPTH_BUFFER_BIT);
+        TARNativeInterface.nativeRender();
+        mGLSurfaceView.requestRender();
     }
 }
